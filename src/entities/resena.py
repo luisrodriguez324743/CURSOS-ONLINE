7899d131b7a589381f5a6e0033b32f862eb0b877
luisrodriguez324743 <you@example.comluisrodriguez324743@correo.itm.edu.co>
2026-08-28 14:04:13 -0500

    feat: Modifique las entidades y el main

--- src/entities/resena.py
+++ src/entities/resena.py
@@ -6,9 +6,8 @@ from uuid import UUID, uuid4
 @dataclass
 class Resena:
     id_resena: UUID = field(default_factory=uuid4)
-    calificacion: int = 0
+    calificacion: int = 5
     comentario: str = ""
     fecha: datetime = field(default_factory=datetime.now)
     id_usuario: UUID | None = None
     id_curso: UUID | None = None
-    id_rol: UUID | None = None
--- src/entities/rol.py
+++ src/entities/rol.py
@@ -1,18 +1,12 @@
 from dataclasses import dataclass, field
-from datetime import datetime
 from uuid import UUID, uuid4
 
 
 @dataclass
-class Usuario:
-    id_usuario: UUID = field(default_factory=uuid4)
+class Rol:
+    id_rol: UUID = field(default_factory=uuid4)
     nombre: str = ""
-    correo: str = ""
-    id_rol: UUID = None  # referencia al Rol (FK)
-    area: str = ""
-    activo: bool = True
-    fecha_registro: datetime = field(default_factory=datetime.now)
+    descripcion: str = ""
 
     def __str__(self) -> str:
-        estado = "activo" if self.activo else "inactivo"
-        return f"Usuario({self.nombre}, {self.correo}, área={self.area}, {estado})"
+        return f"Rol({self.nombre})"
